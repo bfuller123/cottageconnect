5d6d1af903cae1c9b0c34fea2f071fa2d48a34a1
Brett Fuller <bfuller123@gmail.com>
2017-12-18 23:19:17 -0600

    changed list to cards

--- src/components/list.js
+++ src/components/list.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-
 class List extends React.Component {
   render() {
-    return(<li>{this.props.name}</li>);
+    return(
+      <div className="card">
+        <img className="cardImage" src={this.props.image} />
+        <div className="cardDetails">
+          <h2 className="cardName">{this.props.name}</h2>
+          <h4 className="cardAddress">{this.props.address}</h4>
+        </div>
+      </div>
+    )
   }
 }
 
