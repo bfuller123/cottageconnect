48b29639513e17c43237581526bb55ab4aec75f1
Brett Fuller <bfuller123@gmail.com>
2017-12-19 22:13:14 -0600

    moved search bar and made inline

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -4,8 +4,11 @@ class Searchbar extends React.Component {
   render() {
     return(
       <form>
-        <input type="text" id="searchInput" placeholder="city"/>
-        <input type="submit" id="searchSubmit" value="search" />
+        <h3 className="search seachHeader">Food</h3>
+        <input type="text" className="search searchInput" id="searchFood" placeholder="ex. cookies" />
+        <h3 className="search seachHeader">Area</h3>
+        <input type="text" className="search searchInput" id="searchCity" placeholder="ex. Dallas, TX or 75202" />
+        <input type="submit" className="search searchButton" id="searchSubmit" value="search" />
       </form>
     )
   }
