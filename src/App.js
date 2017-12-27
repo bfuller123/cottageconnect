591bd0e0637c33b6011b0127ad7e506784d249b1
Brett Fuller <bfuller123@gmail.com>
2017-12-26 23:36:30 -0600

    made customer portal. need to move to its own page in the future

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Cardloader from './components/cardloader'
-import Searchbar from './components/searchbar'
+import Cardloader from './components/cardloader';
+import Searchbar from './components/searchbar';
+import PortalLoader from './components/portalloader';
+
+// TODO: Move PortalLoader to its own page!
 
 class App extends Component {
   render() {
@@ -10,9 +13,12 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Cottage Connect</h1>
           <h3>Connecting People with Homemade Products</h3>
+          <button id="sign-up btn">Sign Up</button>
+          <button id="sign-in btn">Sign In</button>
           <Searchbar />
         </header>
         <Cardloader />
+        <PortalLoader />
       </div>
     );
   }
