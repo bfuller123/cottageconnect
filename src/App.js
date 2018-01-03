fb36a88b774f0105db506a6af00df666c650b884
Brett Fuller <bfuller123@gmail.com>
2018-01-02 21:04:15 -0600

    frontend router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Main from "./components/Main";
-import About from "./components/About";
-import Search from "./components/Search";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import MemberPortal from "./pages/MemberPortal";
 import Footer from "./components/Footer";
 
 import "./App.css";
@@ -11,13 +11,15 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
+      <Router>
       <div>
         <Navbar />
-        <Main />
-        <About />
-        <Search />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/portal" component={MemberPortal} />
         <Footer />
-    </div>
+      </div>
+      </Router>
     );
   }
 }
