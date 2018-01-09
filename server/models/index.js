4045a353141b13ceb371f2aaf5676e4166e4e2d0
Brett Fuller <bfuller123@gmail.com>
2018-01-09 13:43:34 -0600

    for heroku push setting server/routes/index.js back

--- server/models/index.js
+++ server/models/index.js
@@ -5,7 +5,7 @@ const herokuMongoURI = 'mongodb://heroku_wkwxbdvp:rb7p43plsgl2updmho1hvh77d1@ds2
 module.exports.connect = (uri) => {
 
   //if this doesn't work, delete uri = herokuMongoURI;
-  // uri = herokuMongoURI;
+  uri = herokuMongoURI;
   mongoose.connect(uri);
   // plug in the promise library:
   mongoose.Promise = Promise;
