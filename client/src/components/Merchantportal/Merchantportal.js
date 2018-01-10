85895c957101e62c5b872146570724b420273250
Brett Fuller <bfuller123@gmail.com>
2018-01-10 17:18:51 -0600

    merchant portal can be updated

--- client/src/components/Merchantportal/Merchantportal.js
+++ client/src/components/Merchantportal/Merchantportal.js
@@ -17,18 +17,22 @@ class MerchantPortal extends React.Component {
         <div className="row">
           <div className="col-lg-6 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-12">
             <h2 className="MHede">Welcome, {this.props.merchant}</h2>
-            <h4 className="MType">Shop Address:  <input type="text" value={this.props.address} id="UserAddress" /></h4>
-            <h4 className="MType">Shop Email:  <input type="text" value={this.props.email} id="UserEmail" /></h4>
+            <h4 className="MType">Shop Street Address:  <input type="text" onChange={this.props.inputChangeHandler} data-group="address" data-attribute="streetAddress1" value={this.props.address.streetAddress1} id="UserAddress" /></h4>
+            <h4 className="MType">Shop Apt/Unit #:  <input type="text" onChange={this.props.inputChangeHandler} data-group="address" data-attribute="streetAddress2" value={this.props.address.streetAddress2} id="UserAddress" /></h4>
+            <h4 className="MType">Shop City:  <input type="text" onChange={this.props.inputChangeHandler} data-group="address" data-attribute="city" value={this.props.address.city} id="UserAddress" /></h4>
+            <h4 className="MType">Shop State:  <input type="text" onChange={this.props.inputChangeHandler} data-group="address" data-attribute="state" value={this.props.address.state} id="UserAddress" /></h4>
+            <h4 className="MType">Shop Zip Code:  <input type="text" onChange={this.props.inputChangeHandler} data-group="address" data-attribute="zipCode" value={this.props.address.zipCode} id="UserAddress" /></h4>
+            <h4 className="MType">Shop Email:  <input type="text" onChange={this.props.inputChangeHandler} data-group="user" data-attribute="email" value={this.props.email} id="UserEmail" /></h4>
             <h4 className="MType">Type of Shop:
               <ul>
                 {categories}
               </ul></h4>
-              <button id="addNewCategory" className="button" onClick={this.props.addCategoryBtnClick}>Add Types</button>
+              <button id="categories" className="button" onClick={this.props.addCategoryBtnClick}>Add Types</button>
             <h4 className="MType">Products for Sale:
               <ul>
               {goods}
               </ul></h4>
-              <button id="addNewGood" className="button" onClick={this.props.addGoodBtnClick}>Add Products</button>
+              <button id="goods" className="button" onClick={this.props.addGoodBtnClick}>Add Products</button>
           </div>
         </div>
 
