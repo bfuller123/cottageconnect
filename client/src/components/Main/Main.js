8f81e77ade7dd7cea45526fd01e147bba71e6008
Kim Kaufman <kimgomizzou@hotmail.com>
2018-01-10 16:12:05 -0600

    landing page pic responsive

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,29 +1,18 @@
 import React, { Component } from "react";
 import "./Main.css";
-import Background from "./../../images/cover2.jpg";
-
-var mainStyle = {
-  width: "100%",
-  height: "1000px",
-  backgroundImage: "url(" + Background + ")",
-};
 
 class MainTwo extends Component {
   render() {
     return (
-      <section style={ mainStyle }>
         <div className="row">
-          <div className="MainHeads">
-            <h1 className="MainH1">Cottage Connect</h1>
-            <h2 className="MainH2">Uniting people with homemade products</h2>
-              <div className="container-fluid text-center">
+          <div className="container-fluid text-center col-lg-12 col-md-12 col-sm-12">
+            <img src={require("./../../images/coverCenter.png")} className="img-responsive" alt="Responsive image" />
+
                 <a href="/#About" title="To About">
                 <span className="glyphicon glyphicon-circle-arrow-down"></span></a>
 
-              </div>
           </div>
         </div>
-      </section>
     );
   }
 }
