51e7f3352af0fa0ff84b76dacbad802078de32f3
Brett Fuller <bfuller123@gmail.com>
2018-01-11 00:21:39 -0600

    email form on landing page

---
+++ client/src/components/Emailform/EmailForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './EmailForm.css';
+
+const EmailForm = (props) => {
+  return (
+    <div className="invisible" id="emailForm">
+      <h2>Contact The Cottage</h2>
+      <form>
+        <h4>To: {props.to}</h4>
+        <h4>From: <input type="text" id="emailFrom" placeholder="Your Email"/></h4>
+        <h4><input type="text" id="emailSubject" placeholder="Subject Line" /></h4>
+        <br />
+        <textarea id="emailBody"> </textarea>
+        <br />
+        <button onClick={props.clickHandler} id="emailSendButton">Send</button>
+      </form>
+    </div>
+  )
+}
+
+export default EmailForm;
